2ca7e78eab1531d620e5cf767b0d87681766fb0a
core23 <mail@core23.de>
2018-12-19 22:29:07 +0100

    Removed deprecation warnings

--- src/Form/Extension/TimeFormExtension.php
+++ src/Form/Extension/TimeFormExtension.php
@@ -99,8 +99,10 @@ final class TimeFormExtension extends AbstractTypeExtension
     /**
      * {@inheritdoc}
      */
-    public function getExtendedType()
+    public static function getExtendedTypes(): iterable
     {
-        return FormType::class;
+        return [
+            FormType::class,
+            ];
     }
 }
