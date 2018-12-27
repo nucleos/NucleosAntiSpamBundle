51d43c2fd179ebf12ee1805a8fa103c7fdd71eb0
core23 <mail@core23.de>
2018-12-27 08:09:41 +0100

    Use asserts for type safe root config nodes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -24,13 +24,13 @@ final class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('core23_antispam');
 
-        /* @var ArrayNodeDefinition $rootNode */
         // Keep compatibility with symfony/config < 4.2
         if (!\method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->root('core23_antispam');
         } else {
             $rootNode = $treeBuilder->getRootNode();
         }
+        \assert($rootNode instanceof ArrayNodeDefinition);
 
         $this->addTwigSection($rootNode);
         $this->addTimeSection($rootNode);
