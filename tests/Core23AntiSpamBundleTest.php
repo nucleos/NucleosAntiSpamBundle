8e79a89f0d951a54fcb044fb6b7b9bad01d45174
core23 <mail@core23.de>
2019-03-18 22:14:04 +0100

    [BUILD] Added more tests

---
+++ tests/Core23AntiSpamBundleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\AntiSpamBundle\Tests;
+
+use Core23\AntiSpamBundle\Core23AntiSpamBundle;
+use Core23\AntiSpamBundle\DependencyInjection\Core23AntiSpamExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+class Core23AntiSpamBundleTest extends TestCase
+{
+    public function testItIsInstantiable(): void
+    {
+        $bundle = new Core23AntiSpamBundle();
+
+        $this->assertInstanceOf(BundleInterface::class, $bundle);
+    }
+
+    public function testGetContainerExtension(): void
+    {
+        $bundle = new Core23AntiSpamBundle();
+
+        $this->assertInstanceOf(Core23AntiSpamExtension::class, $bundle->getContainerExtension());
+    }
+}
