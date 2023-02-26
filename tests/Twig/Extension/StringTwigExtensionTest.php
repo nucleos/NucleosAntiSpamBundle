751370649747f6ba59ead91b077a9588c8bc553c
core23 <mail@core23.de>
2023-02-26 11:45:13 +0100

    Use deterministic mail replacement

--- tests/Twig/Extension/StringTwigExtensionTest.php
+++ tests/Twig/Extension/StringTwigExtensionTest.php
@@ -21,7 +21,7 @@ final class StringTwigExtensionTest extends TestCase
 {
     public function testGetFilters(): void
     {
-        $extension = new StringTwigExtension('spam', ['[AT]'], ['[DOT]']);
+        $extension = new StringTwigExtension('spam', ['[AT]', '[ÄT]', '(AT)', '|AT|'], ['[DOT]', ' PUNKT ', '[.]']);
 
         $filters = $extension->getFilters();
 
@@ -38,7 +38,7 @@ final class StringTwigExtensionTest extends TestCase
      */
     public function testAntispam(string $input, string $output): void
     {
-        $extension = new StringTwigExtension('spam', ['[AT]'], ['[DOT]']);
+        $extension = new StringTwigExtension('spam', ['[AT]', '[ÄT]', '(AT)', '|AT|'], ['[DOT]', ' PUNKT ', '[.]']);
 
         static::assertSame($output, $extension->antispam($input));
     }
@@ -48,7 +48,7 @@ final class StringTwigExtensionTest extends TestCase
      */
     public function testAntispamText(string $input, string $output): void
     {
-        $extension = new StringTwigExtension('spam', ['[AT]'], ['[DOT]']);
+        $extension = new StringTwigExtension('spam', ['[AT]', '[ÄT]', '(AT)', '|AT|'], ['[DOT]', ' PUNKT ', '[.]']);
 
         static::assertSame($output, $extension->antispam($input, false));
     }
@@ -61,22 +61,17 @@ final class StringTwigExtensionTest extends TestCase
                 'Lorem Ipsum <script>const link = "foo@bar.baz"; </script> Sit Amet',
                 'Lorem Ipsum <script>const link = "foo@bar.baz"; </script> Sit Amet',
             ],
-            // TODO: Replace plain mails text in html
-            //            [
-            //                'Lorem Ipsum foo.sub@bar.baz.tld Sit Amet',
-            //                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>[AT]<span>bar[DOT]baz[DOT]tld</span></span> Sit Amet',
-            //            ],
             [
                 'Lorem Ipsum <a href="mailto:john@smith.cool">John Smith</a> Sit Amet',
-                'Lorem Ipsum <span class="spam"><span>john</span>[AT]<span>smith[DOT]cool</span> (<span>John Smith</span>)</span> Sit Amet',
+                'Lorem Ipsum <span class="spam"><span>john</span>[AT]<span>smith[.]cool</span> (<span>John Smith</span>)</span> Sit Amet',
             ],
             [
                 'Lorem Ipsum <a href="mailto:foo.sub@bar.baz.tld">foo.sub@bar.baz.tld</a> Sit Amet',
-                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>[AT]<span>bar[DOT]baz[DOT]tld</span></span> Sit Amet',
+                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>(AT)<span>bar PUNKT baz PUNKT tld</span></span> Sit Amet',
             ],
             [
-                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>[AT]<span>bar[DOT]baz[DOT]tld</span></span> Sit Amet',
-                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>[AT]<span>bar[DOT]baz[DOT]tld</span></span> Sit Amet',
+                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>(AT)<span>bar PUNKT baz PUNKT tld</span></span> Sit Amet',
+                'Lorem Ipsum <span class="spam"><span>foo[DOT]sub</span>(AT)<span>bar PUNKT baz PUNKT tld</span></span> Sit Amet',
             ],
         ];
     }
@@ -89,11 +84,11 @@ final class StringTwigExtensionTest extends TestCase
         return [
             [
                 'Lorem Ipsum foo.sub@bar.baz.tld Sit Amet',
-                'Lorem Ipsum foo[DOT]sub[AT]bar[DOT]baz[DOT]tld Sit Amet',
+                'Lorem Ipsum foo[DOT]sub[AT]bar PUNKT baz PUNKT tld Sit Amet',
             ],
             [
-                'Lorem Ipsum foo[DOT]sub[AT]bar[DOT]baz[DOT]tld Sit Amet',
-                'Lorem Ipsum foo[DOT]sub[AT]bar[DOT]baz[DOT]tld Sit Amet',
+                'Lorem Ipsum foo[DOT]sub[AT]bar PUNKT baz PUNKT tld Sit Amet',
+                'Lorem Ipsum foo[DOT]sub[AT]bar PUNKT baz PUNKT tld Sit Amet',
             ],
         ];
     }
